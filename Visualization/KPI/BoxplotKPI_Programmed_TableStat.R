e0942c694f20ea25df3894835db6b2cc26a35c71
jan-vanroozendaal <63795264+jan-vanroozendaal@users.noreply.github.com>
2020-05-08 13:47:20 +0200

    Update BoxplotKPI_Programmed_TableStat.R

--- Visualization/KPI/BoxplotKPI_Programmed_TableStat.R
+++ Visualization/KPI/BoxplotKPI_Programmed_TableStat.R
@@ -53,5 +53,6 @@ for (i in 1:length(models)) {
   file_name_part2 <- paste(file_name_part1, "ProgrammedKPI", sep="")
   file_name_part3 <- paste(file_name_part2, ".csv", sep="")
   
-  write.csv(df_result, paste("C:\\Users\\janvr\\Desktop\\KPITables\\", file_name_part3, sep=""), row.names = FALSE)
+  your_directory <-"C:\\Users\\..." #edit file location here
+  write.csv(df_result, paste(your_directory, file_name_part3, sep=""), row.names = FALSE)
 }
