a679fa925ff161ce8bc44acf466239d9bfca9e7e
jan-vanroozendaal <63795264+jan-vanroozendaal@users.noreply.github.com>
2020-05-08 13:40:10 +0200

    Update and rename Analysis_XZ_V2.R to Analysis_XZ.R

--- Visualization/Edges/Analysis_XZ.R
+++ Visualization/Edges/Analysis_XZ.R
@@ -240,6 +240,6 @@ for (i in 1:length(models)) {
   file_name_part2 <- paste(file_name_part1, "X-Z_V2", sep="")
   file_name_part3 <- paste(file_name_part2, ".csv", sep="")
     
-    
-  write.csv(df_result, paste("C:\\Users\\janvr\\Desktop\\Visualization\\Edges\\X-Z\\", file_name_part3, sep=""), row.names = FALSE)
+  your_directory <-"C:\\Users\\..." #edit file location here
+  write.csv(df_result, paste(your_directory, file_name_part3, sep=""), row.names = FALSE)
 }
