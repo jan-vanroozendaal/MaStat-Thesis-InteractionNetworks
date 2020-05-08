7fb243d28f5e79c7b62bf60854692916f01cad93
jan-vanroozendaal <63795264+jan-vanroozendaal@users.noreply.github.com>
2020-05-08 13:46:05 +0200

    Add files via upload

---
+++ Visualization/KPI/BoxplotKPI_InCluster.R
@@ -0,0 +1,49 @@
+#=-=-=-=-=-=-=-=-=-=-=
+df_total$specificity <- as.numeric(as.character(df_total$specificity))
+df_total$sensitivity <- as.numeric(as.character(df_total$sensitivity))
+df_total$`%_programmed` <- as.numeric(as.character(df_total$`%_programmed`))
+df_total$`%_incluster` <- as.numeric(as.character(df_total$`%_incluster`))
+df_total$size <- as.numeric(as.character(df_total$size))
+
+
+#change the order of the factor df_total$threshold
+df_total$threshold <- factor(df_total$threshold , levels=c("False", "0.005", "0.01", "0.015", "0.02", "0.025",
+                                                           "0.03", "0.035", "0.04", "0.045", "0.05", "True"))
+
+#change the order of the factor df_total$case
+df_total$case <- factor(df_total$case , levels=c("Syn", "Zero", "Redun"))
+
+library(ggplot2)
+bp <- ggplot(df_total[df_total$model == "Model 2"
+                      | df_total$model == "Model 3"
+                      | df_total$model == "Model 6",], aes(x=threshold, y=`%_incluster`, group=threshold)) + 
+  geom_boxplot(aes(fill=threshold))
+
+# Split in horizontal direction
+bp + scale_y_continuous(breaks = seq(0, 1, 0.5), limits = c(0,1)) + 
+  labs(x = "Threshold Level", y = "% in cluster (scaled 0-1)", fill="Threshold Level") +
+  #format axis labels and ticks
+  theme(
+    axis.title.x = element_text(size=14),
+    axis.title.y = element_text(size=14),
+    axis.text.x = element_text(size=8, angle=315),
+    axis.text.y = element_text(size=10)) +
+  
+  #perform the facet grid    
+  facet_grid(model ~ case) +
+  
+  #format facet labels  
+  theme(
+    strip.text.x = element_text(
+      size = 14, face = "bold"
+    ),
+    strip.text.y = element_text(
+      size = 14, face = "bold"
+    )
+  )
+
+
+
+
+
+
---
+++ Visualization/KPI/BoxplotKPI_InCluster_TableStat.R
@@ -0,0 +1,59 @@
+df_total$specificity <- as.numeric(as.character(df_total$specificity))
+df_total$sensitivity <- as.numeric(as.character(df_total$sensitivity))
+df_total$`%_programmed` <- as.numeric(as.character(df_total$`%_programmed`))
+df_total$`%_incluster` <- as.numeric(as.character(df_total$`%_incluster`))
+df_total$size <- as.numeric(as.character(df_total$size))
+
+
+#change the order of the factor df_total$threshold
+df_total$threshold <- factor(df_total$threshold , levels=c("False", "0.005", "0.01", "0.015", "0.02", "0.025",
+                                                           "0.03", "0.035", "0.04", "0.045", "0.05", "True"))
+
+#change the order of the factor df_total$case
+df_total$case <- factor(df_total$case , levels=c("Syn", "Zero", "Redun"))
+
+#subset by Model 2, 3, and 6
+df_total <- df_total[df_total$model == "Model 2" | df_total$model == "Model 3" | df_total$model == "Model 6",]
+
+median_col <- aggregate(df_total$`%_incluster`, by = list(df_total$model, df_total$case, df_total$threshold), median)
+Q1_col <- aggregate(df_total$`%_incluster`, by = list(df_total$model, df_total$case, df_total$threshold), quantile, probs = 0.25)
+Q3_col <- aggregate(df_total$`%_incluster`, by = list(df_total$model, df_total$case, df_total$threshold), quantile, probs = 0.75)
+
+Merge1 <- merge(x = median_col, y = Q1_col, by = c("Group.1", "Group.2", "Group.3"), all.x = TRUE)
+Merge2 <- merge(x = Merge1, y = Q3_col, by = c("Group.1", "Group.2", "Group.3"), all.x = TRUE)
+colnames(Merge2) <- c("Model", "Case", "Threshold", "Median", "Q1", "Q3")
+
+#create summary statistics
+Merge2$Median <- format(round(Merge2$Median, 4), nsmall = 4)
+Merge2$Q1 <- format(round(Merge2$Q1, 4), nsmall = 4)
+Merge2$Q3 <- format(round(Merge2$Q3, 4), nsmall = 4)
+
+#multiply by 100 to get actual percentages (improves readability)
+Merge2$Median <- as.numeric(Merge2$Median) * 100
+Merge2$Q1 <- as.numeric(Merge2$Q1) * 100
+Merge2$Q3 <- as.numeric(Merge2$Q3) * 100
+
+#create dynamic sentence using summary statistics per entry
+sentence_part1 <- paste(trimws(as.character(format(round(Merge2$Median, digits=2)), nsmall=2)), " (", sep = "")
+sentence_part2 <- paste(sentence_part1, trimws(as.character(format(round(Merge2$Q1, digits=2)), nsmall=2)), sep= "")
+sentence_part3 <- paste(sentence_part2, "-", sep = "")
+sentence_part4 <- paste(sentence_part3, trimws(as.character(format(round(Merge2$Q3, digits=2)), nsmall=2)), sep = "")
+sentence_part5 <- paste(sentence_part4, ")", sep = "")
+Merge2$sentence <- sentence_part5
+
+#pivot data
+library(reshape)
+
+#for each combination of model/case/threshold, pivot data, and write separate .csv files per model
+models <- unique(Merge2$Model)
+
+for (i in 1:length(models)) {
+  df_result <- Merge2[Merge2$Model == models[i], c(2,3,7)]
+  df_result <- cast(df_result, Threshold ~ Case)
+  
+  file_name_part1 <- paste(models[i], "_", sep="")
+  file_name_part2 <- paste(file_name_part1, "ClusterKPI", sep="")
+  file_name_part3 <- paste(file_name_part2, ".csv", sep="")
+  
+  write.csv(df_result, paste("C:\\Users\\janvr\\Desktop\\KPITables\\", file_name_part3, sep=""), row.names = FALSE)
+}
